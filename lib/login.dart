e25d46cf13ab83c5a8d3ff8a17232d6a7a32b4d1
LabibZabaneh <labibraedzabaneh@gmail.com>
2023-09-17 20:05:11 +0300

    Added login page file

---
+++ lib/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  final VoidCallback clickMultiPlayer;
+
+  const Login({super.key, required this.clickMultiPlayer});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 40),
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30)
+            ),
+            child: const Text("Welcome!", style: TextStyle(fontSize: 20)),
+          ),
+        ),
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.only(top: 50),
+              padding: const EdgeInsets.all(7),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: const Text("Single Player Mode"),
+            ),
+          ),
+        ),
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              widget.clickMultiPlayer();
+            },
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.all(7),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: const Text(" Multi Player Mode "),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
