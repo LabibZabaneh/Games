522a79bc29bd57d1d749f667ae3d6e5e2b37355d
LabibZabaneh <labibraedzabaneh@gmail.com>
2023-09-28 19:23:35 +0100

    Added connect4Circle.dart file

---
+++ lib/widgets/connect4Circle.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Connect4Circle extends StatelessWidget {
+  final bool isEmpty;
+  final bool value;
+  final int row;
+  final int column;
+  final Function(int row, int column) move;
+
+  Connect4Circle({super.key, required this.row, required this.column, required this.move,required this.isEmpty, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (isEmpty){
+          move(row, column);
+        }
+      },
+      child: Container(
+        width: 45,
+        height: 45,
+        decoration: BoxDecoration(
+            color: isEmpty ? Colors.white : (value ? Colors.blue : Colors.amber),
+            borderRadius: BorderRadius.circular(100)
+        ),
+      ),
+    );
+  }
+}
