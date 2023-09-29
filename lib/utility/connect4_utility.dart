6c62ead768f0635e2543763509d85226950ce603
LabibZabaneh <labibraedzabaneh@gmail.com>
2023-09-29 15:13:27 +0100

    Fixed connect4 gameover reverse diagonal bug

--- lib/utility/connect4_utility.dart
+++ lib/utility/connect4_utility.dart
@@ -21,8 +21,10 @@ class Connect4Utility {
       return [[row, column], [row, column+1], [row, column+2], [row, column+3]];
     } else  if (checkVertical(value, row, column)){
       return [[row, column], [row+1, column], [row+2, column], [row+3, column]];
-    } else  if (checkDiagonal(value, row, column)){
+    } else  if (checkDiagonal(value, row, column, true)){
       return [[row, column], [row+1, column+1], [row+2, column+2], [row+3, column+3]];
+    } else  if (checkDiagonal(value, row, column, false)){
+      return [[row, column], [row+1, column-1], [row+2, column-2], [row+3, column-3]];
     }
     return [];
   }
@@ -65,14 +67,20 @@ class Connect4Utility {
     return false;
   }
 
-  static bool checkDiagonal(List<List<int>> values, int row, int column){
-    if (values.length-row<4 || values.length-column<4){
-      return false;
+  static bool checkDiagonal(List<List<int>> values, int row, int column, bool side){
+    if (side){
+      if (values.length-row<4 || values.length-column<4){
+        return false;
+      }
+    } else {
+      if (row > 1 || column < 1){
+        return false;
+      }
     }
     int i = 1;
     int value = values[row][column];
     while (i < 4){
-      if (value == values[row+i][column+i]){
+      if (value == values[row+i][side ? column+i : column-i]){
         if (i == 3){
           return true;
         }
